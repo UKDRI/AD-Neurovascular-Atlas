e538939277442283b5e78970d286275bc448648e
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2023-06-23 15:18:26 +0100

    Add scripts for processing scflow with seurat

---
+++ 04_data_analysis/990_code_libraries/scflow-seurat-processing.R
@@ -0,0 +1,28 @@
+# load packages
+library(Seurat)
+library(future)
+
+# read data
+seurat <- readr::read_rds(here::here("03_data/990_processed_data/001_snrnaseq/07_scflow_analysis/scflow-seurat-preprocessing.rds"))
+
+## number of cores
+n_workers <- 30
+## 200 GB
+options(future.globals.maxSize = 200000 * 1024^2)
+plan(strategy = "multicore", workers = n_workers)
+
+## normalisation
+seurat <- NormalizeData(object = seurat, normalization.method = "LogNormalize", scale.factor = 10000)
+
+## variable genes
+seurat <- FindVariableFeatures(seurat, selection.method = "vst", nfeatures = 2000)
+
+## scaling
+all.genes <- rownames(seurat)
+seurat <- ScaleData(seurat, features = all.genes)
+
+## PCA
+seurat <- RunPCA(seurat, features = VariableFeatures(object = seurat))
+
+# Save
+readr::write_rds(seurat, here::here("03_data/990_processed_data/001_snrnaseq/07_scflow_analysis/scflow-seurat-postprocessing.rds"))
