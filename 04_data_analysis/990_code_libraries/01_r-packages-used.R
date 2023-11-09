6545beecc8b7b73490f4d67604a4d82d4da4378d
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2023-11-09 16:31:49 +0000

    Update package install/load scripts
    Updated to include ability to handle github packages via devtools

--- 04_data_analysis/990_code_libraries/01_r-packages-used.R
+++ 04_data_analysis/990_code_libraries/01_r-packages-used.R
@@ -5,12 +5,12 @@
 pkg <- c("helpers" = c("stringr", "DT", "reshape", "dplyr", "tidyr",
                        "tibble", "purrr", "here", "readr", "kableExtra", 
                        "janitor", "labelled", "BiocManager", "data.table",
-                       "gt", "fgsea", "europepmc", "readxl"),
+                       "gt", "europepmc", "readxl", "NMF"),
          "website" = c("downlit", "xml2"),
          "plots" = c("corrgram", "RColorBrewer", "pheatmap", "ggplot2",
                      "ggsci", "cowplot", "scales", "ggvenn", "ggpubr", 
                      "gridExtra", "tidytext", "viridis", "wesanderson",
-                     "pals"))
+                     "pals", "ggalluvial"))
 
 pkg_bioconductor <- c("cellity", "scater", "Seurat", "SC3", "DESeq2", "edgeR",
                       "BiocParallel", "org.Hs.eg.db", "pathview", "gage",
@@ -19,10 +19,12 @@ pkg_bioconductor <- c("cellity", "scater", "Seurat", "SC3", "DESeq2", "edgeR",
                       "PCAtools", "batchelor", "bluster", "igraph", 
                       "SingleCellExperiment", "scRNAseq", "biomaRt",
                       "GEOquery", "AnnotationDbi", "clusterProfiler",
-                      "DOSE", "enrichplot", "EnsDb.Hsapiens.v79")
+                      "DOSE", "enrichplot", "EnsDb.Hsapiens.v79", "fgsea")
+
+pkg_devtools <- c("sqjin/CellChat")
 
 ## create function to installing any missing packages and then load them all
-sort_packages <- function(packages, bioconductor = FALSE) {
+sort_packages <- function(packages, bioconductor = FALSE, devtools = FALSE) {
     ## Check if packages are not installed and assign the
     ## names of the packages not installed to the variable new.pkg
     new_pkg <- packages[!(packages %in% installed.packages())]
@@ -33,6 +35,12 @@ sort_packages <- function(packages, bioconductor = FALSE) {
         install.packages(new_pkg, repos = "https://cran.rstudio.com")
     } else if (length(new_pkg) & bioconductor) {
         BiocManager::install(new_pkg)
+    } else if (length(new_pkg) & devtools) {
+      devtools::install_github(new_pkg)
+    }
+    
+    if (devtools) {
+      packages <- sapply(strsplit(packages, "/"), tail, n = 1)
     }
     ## load packages
     lapply(packages, require, character.only = TRUE)
@@ -40,10 +48,11 @@ sort_packages <- function(packages, bioconductor = FALSE) {
 ## use function
 sort_packages(pkg)
 sort_packages(pkg_bioconductor, bioconductor = TRUE)
+sort_packages(pkg_devtools, devtools = TRUE)
 
 
 
 
 
-rm(pkg, pkg_bioconductor, sort_packages)
+rm(pkg, pkg_bioconductor, pkg_devtools, sort_packages)
 
