b0a9b58b6d36bca93b5c68b5271870739eeb4c3e
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2024-02-12 16:52:56 +0000

    Update SNP files
    There wasn't the "N" column in the SNP file, so I've added that

--- 04_data_analysis/001_snrnaseq_analysis/18_step2_magma.sh
+++ 04_data_analysis/001_snrnaseq_analysis/18_step2_magma.sh
@@ -39,79 +39,79 @@ cd $WORK_DIR
 
 #### Without proxies and with APOE - 35k10k window
 
-mkdir temp_annot_35k10k # make a temporary directory to host the intermediate files
-
-# PLINK files from reference (.bed/.bim/.fam)
-Data_File=${WORK_DIR}"g1000_eur"
-# Output of step 1
-Annot_File=${WORK_DIR}"NCBI37_annotated_window_35k10k.genes.annot"
-# GWAS summary stats
-# Ata has summary stats here: /scratch/scw1751/shared/Bellenguez_2022_sumstats/
-# GCST90027158_buildGRCh38_noATGC.tsv is for build 38, but the MAGMA site has reference data for build 37 only, asking sam for link to 38
-# Should try with and without APOE in summary stats
-# Should also try with and without proxy cases, file: EADB_release_Feb2020.meta_model_pcs.mac_info_20_GC_OFF.formatted.exc_perc_cases_50.het_5e-8.freq_amp_40_noATGC_GRCh37.tsv
-SNP_Pval_File="/scratch/scw1751/shared/Bellenguez_2022_sumstats/EADB_release_Feb2020.meta_model_pcs.mac_info_20_GC_OFF.formatted.exc_perc_cases_50.het_5e-8.freq_amp_40_noATGC_GRCh37.tsv"
-
-Output_Prefix="EUROPE_noproxy_35k10k"
-
-    # run magma in parallel, 10 threads in this case
-
-parallel magma \
-   --batch {} 10 \
-   --bfile $Data_File \
-   --gene-annot $Annot_File \
-   --gene-model snp-wise=mean \
-   --pval $SNP_Pval_File ncol=N \
-   --out temp_annot_35k10k/$Output_Prefix \
-::: {1..10}
-
-# merge all intermediate files generated under the temp_annot files
-# and send out for one single file set
-
-magma \
-   --merge temp_annot_35k10k/$Output_Prefix \
-   --out temp_annot_35k10k/$Output_Prefix
-
-# extract merged files for subsequent analysis
-
-cp ./temp_annot_35k10k/$Output_Prefix.genes.* .
-
-# remove the temporary directory
-
-rm -r temp_annot_35k10k
-
-#### Without proxies and with APOE - no window
-
-mkdir temp_annot # make a temporary directory to host the intermediate files
-
-Annot_File=${WORK_DIR}"NCBI37_annotated_nowindow.genes.annot"
-SNP_Pval_File="/scratch/scw1751/shared/Bellenguez_2022_sumstats/EADB_release_Feb2020.meta_model_pcs.mac_info_20_GC_OFF.formatted.exc_perc_cases_50.het_5e-8.freq_amp_40_noATGC_GRCh37.tsv"
-Output_Prefix="EUROPE_noproxy_nowindow"
-
-# run magma in parallel, 10 threads in this case
-parallel magma \
-   --batch {} 10 \
-   --bfile $Data_File \
-   --gene-annot $Annot_File \
-   --gene-model snp-wise=mean \
-   --pval $SNP_Pval_File ncol=N \
-   --out temp_annot/$Output_Prefix \
-::: {1..10}
-
-# merge all intermediate files generated under the temp_annot files
-# and send out for one single file set
-
-magma \
-   --merge temp_annot/$Output_Prefix \
-   --out temp_annot/$Output_Prefix
-
-# extract merged files for subsequent analysis
-
-cp ./temp_annot/$Output_Prefix.genes.* .
-
-# remove the temporary directory
-
-rm -r temp_annot
+# mkdir temp_annot_35k10k # make a temporary directory to host the intermediate files
+# 
+# # PLINK files from reference (.bed/.bim/.fam)
+# Data_File=${WORK_DIR}"g1000_eur"
+# # Output of step 1
+# Annot_File=${WORK_DIR}"NCBI37_annotated_window_35k10k.genes.annot"
+# # GWAS summary stats
+# # Ata has summary stats here: /scratch/scw1751/shared/Bellenguez_2022_sumstats/
+# # GCST90027158_buildGRCh38_noATGC.tsv is for build 38, but the MAGMA site has reference data for build 37 only, asking sam for link to 38
+# # Should try with and without APOE in summary stats
+# # Should also try with and without proxy cases, file: EADB_release_Feb2020.meta_model_pcs.mac_info_20_GC_OFF.formatted.exc_perc_cases_50.het_5e-8.freq_amp_40_noATGC_GRCh37.tsv
+# SNP_Pval_File="/scratch/scw1751/shared/Bellenguez_2022_sumstats/EADB_release_Feb2020.meta_model_pcs.mac_info_20_GC_OFF.formatted.exc_perc_cases_50.het_5e-8.freq_amp_40_noATGC_GRCh37.tsv"
+# 
+# Output_Prefix="EUROPE_noproxy_35k10k"
+# 
+#     # run magma in parallel, 10 threads in this case
+# 
+# parallel magma \
+#    --batch {} 10 \
+#    --bfile $Data_File \
+#    --gene-annot $Annot_File \
+#    --gene-model snp-wise=mean \
+#    --pval $SNP_Pval_File ncol=N \
+#    --out temp_annot_35k10k/$Output_Prefix \
+# ::: {1..10}
+# 
+# # merge all intermediate files generated under the temp_annot files
+# # and send out for one single file set
+# 
+# magma \
+#    --merge temp_annot_35k10k/$Output_Prefix \
+#    --out temp_annot_35k10k/$Output_Prefix
+# 
+# # extract merged files for subsequent analysis
+# 
+# cp ./temp_annot_35k10k/$Output_Prefix.genes.* .
+# 
+# # remove the temporary directory
+# 
+# rm -r temp_annot_35k10k
+# 
+# #### Without proxies and with APOE - no window
+# 
+# mkdir temp_annot # make a temporary directory to host the intermediate files
+# 
+# Annot_File=${WORK_DIR}"NCBI37_annotated_nowindow.genes.annot"
+# SNP_Pval_File="/scratch/scw1751/shared/Bellenguez_2022_sumstats/EADB_release_Feb2020.meta_model_pcs.mac_info_20_GC_OFF.formatted.exc_perc_cases_50.het_5e-8.freq_amp_40_noATGC_GRCh37.tsv"
+# Output_Prefix="EUROPE_noproxy_nowindow"
+# 
+# # run magma in parallel, 10 threads in this case
+# parallel magma \
+#    --batch {} 10 \
+#    --bfile $Data_File \
+#    --gene-annot $Annot_File \
+#    --gene-model snp-wise=mean \
+#    --pval $SNP_Pval_File ncol=N \
+#    --out temp_annot/$Output_Prefix \
+# ::: {1..10}
+# 
+# # merge all intermediate files generated under the temp_annot files
+# # and send out for one single file set
+# 
+# magma \
+#    --merge temp_annot/$Output_Prefix \
+#    --out temp_annot/$Output_Prefix
+# 
+# # extract merged files for subsequent analysis
+# 
+# cp ./temp_annot/$Output_Prefix.genes.* .
+# 
+# # remove the temporary directory
+# 
+# rm -r temp_annot
 
 
 #### With proxies and APOE - 35k10k window
@@ -119,7 +119,7 @@ rm -r temp_annot
 mkdir temp_annot_35k10k_nobb # make a temporary directory to host the intermediate files
 
 Annot_File=${WORK_DIR}"NCBI37_annotated_window_35k10k.genes.annot"
-SNP_Pval_File="/scratch/scw1751/shared/Bellenguez_2022_sumstats/GCST90027158_buildGRCh37_noATGC_forMAGMA.tsv"
+SNP_Pval_File=${WORK_DIR}"GCST90027158_buildGRCh37_noATGC_forMAGMA.tsv"
 Output_Prefix="EUROPEUKBB_35k10k"
 
 
@@ -153,7 +153,7 @@ rm -r temp_annot_35k10k_nobb
 mkdir temp_annot_nobb # make a temporary directory to host the intermediate files
 
 Annot_File=${WORK_DIR}"NCBI37_annotated_nowindow.genes.annot"
-SNP_Pval_File="/scratch/scw1751/shared/Bellenguez_2022_sumstats/GCST90027158_buildGRCh37_noATGC_forMAGMA.tsv"
+SNP_Pval_File=${WORK_DIR}"GCST90027158_buildGRCh37_noATGC_forMAGMA.tsv"
 Output_Prefix="EUROPEUKBB_nowindow"
 
 
@@ -188,7 +188,7 @@ rm -r temp_annot_nobb
 mkdir temp_annot_nobb # make a temporary directory to host the intermediate files
 
 Annot_File=${WORK_DIR}"NCBI37_annotated_nowindow.genes.annot"
-SNP_Pval_File="/scratch/scw1751/shared/Bellenguez_2022_sumstats/GCST90027158_buildGRCh37_noATGC_noAPOE_forMAGMA.tsv"
+SNP_Pval_File=${WORK_DIR}"/GCST90027158_buildGRCh37_noATGC_noAPOE_forMAGMA.tsv"
 Output_Prefix="EUROPEUKBB_nowindow_noAPOE"
 
 # run magma in parallel, 10 threads in this case
@@ -221,7 +221,7 @@ rm -r temp_annot_nobb
 mkdir temp_annot_nobb # make a temporary directory to host the intermediate files
 
 Annot_File=${WORK_DIR}"NCBI37_annotated_window_35k10k.genes.annot"
-SNP_Pval_File="/scratch/scw1751/shared/Bellenguez_2022_sumstats/GCST90027158_buildGRCh37_noATGC_noAPOE_forMAGMA.tsv"
+SNP_Pval_File=${WORK_DIR}"/GCST90027158_buildGRCh37_noATGC_noAPOE_forMAGMA.tsv"
 Output_Prefix="EUROPEUKBB_35k10k_noAPOE"
 
 # run magma in parallel, 10 threads in this case
