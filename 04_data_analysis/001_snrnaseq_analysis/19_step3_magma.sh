1408abc270f4fa566e81da6bb73ab68bada21c87
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2024-02-14 13:52:35 +0000

    Rename magma scripts and add step 3

---
+++ 04_data_analysis/001_snrnaseq_analysis/19_step3_magma.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#SBATCH --job-name=MAGMA_step3
+#SBATCH -p c_highmem_dri1
+#SBATCH -o /scratch/c.mpmgb/hawk_output/%x_out_%A_%a_%J.txt
+#SBATCH -e /scratch/c.mpmgb/hawk_output/%x_err_%A_%a_%J.txt
+#SBATCH --time=0-10:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --array=1
+#SBATCH --account=scw1329
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=bernardo-harringtong@cardiff.ac.uk
+
+#####SBATCH --mem=120G
+
+echo "*****************************************************************"
+echo "All jobs in this array have:"
+echo "  - SLURM_ARRAY_JOB_ID: ${SLURM_ARRAY_JOB_ID}"
+echo "  - SLURM_ARRAY_TASK_COUNT: ${SLURM_ARRAY_TASK_COUNT}"
+echo "  - SLURM_ARRAY_TASK_MIN: ${SLURM_ARRAY_TASK_MIN}"
+echo "  - SLURM_ARRAY_TASK_MAX: ${SLURM_ARRAY_TASK_MAX}"
+echo "Job in the array has:"
+echo "    - SLURM_JOB_ID: ${SLURM_JOB_ID}"
+echo "    - SLURM_ARRAY_TASK_ID: ${SLURM_ARRAY_TASK_ID}"
+echo "Host: "`hostname`
+echo "Number of threads (nproc): "`nproc`
+echo "Total memory in GB: "`free -g | grep -oP '\d+' | sed -n 1p`
+echo "Used memory in GB: "`free -g | grep -oP '\d+' | sed -n 2p`
+echo "Free memory in GB: "`free -g | grep -oP '\d+' | sed -n 3p`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo -e "*****************************************************************\n"
+
+module purge
+module load magma/1.10
+module load parallel
+
+
+WORK_DIR="/scratch/scw1329/gmbh/blood-brain-barrier-in-ad/03_data/994_magma_inputs/"
+cd $WORK_DIR
+export WORK_DIR
+
+# Populate Set_Annot_Files array with files matching 'magma_celltypes_*'
+Set_Annot_Files=(${WORK_DIR}/magma_celltypes_*)
+# Populate Gene_Results_Files array with files matching '*.genes.raw'
+Gene_Results_Files=(${WORK_DIR}/*.genes.raw)
+
+# Function to run magma with given files and output prefix
+run_magma() {
+    local Set_Annot_File=$1
+    local Gene_Results_File=$2
+    local annot_name=$(basename "${Set_Annot_File}" .tsv)
+    local gene_name=$(basename "${Gene_Results_File}" .genes.raw)
+    local Output_Prefix="./results/level2_celltypes_${annot_name}_${gene_name}"
+
+    # Run magma command
+    magma \
+        --gene-results $Gene_Results_File \
+        --set-annot $Set_Annot_File \
+        --out $Output_Prefix
+}
+
+# Export the function so it can be used by parallel
+export -f run_magma
+
+# Use parallel to run the combinations
+parallel run_magma ::: "${Set_Annot_Files[@]}" ::: "${Gene_Results_Files[@]}"
+
+
+echo -e "\n*****************************************************************"
+echo "Finished at: "`date`
+echo "*****************************************************************"
--- README.md
+++ README.md
@@ -16,6 +16,20 @@ For set 2 the data was sequenced initially on one Illumina lane, but then more l
 
 The last set is another bath of 40 samples from 20 donors.
 
+## Processing of data
+
+FastQC/MultiQC were run to assess sequencing quality, no issues were found with the latest batch of samples.
+Subsequently standard processing was done with [cellranger](https://www.10xgenomics.com/support/software/cell-ranger/latest), using an updated reference with Ensembl version 109 and Gencode version 43.
+The output of this was run through [scFlow](https://github.com/combiz/nf-core-scflow/tree/dev-nf) with standard parameters which was then read into R via the [Seurat](https://satijalab.org/seurat/) package.
+
+Looking at the read counts and number of features output from scFlow revealed that 3 of the donors, and the vascular fraction from a fourth donor, were of poor quality and so they were excluded from downstream analysis. 
+PCA was performed and 35 dimensions were used to find nearest-neighbors and a resolution of 0.6 was used to find clusters via Seurat.
+UMAP was computed with the same 35 dimensions.
+Cluster markers were found using genes with at least 20% of cells in a cluster expressing them, and a minimum log foldchange of 0.5, again via Seurat.
+
+Differences in celltype proportions between cases and controls were examined with the [speckle](https://github.com/phipsonlab/speckle) R package.
+It leverages biological replication to obtain measures of variability of cell type proportion estimates and uses empirical Bayes to stabilize variance estimates by borrowing information across cell types. 
+
 ## Contributors
 
 - Frank Wessely
