f4e9c0c4a8417a87349f8a7336fbb23fbff272a0
mpmgb <Bernardo-HarringtonG@cardiff.ac.uk>
2024-02-26 11:18:43 +0000

    Add LDscore scripts from Emeka

---
+++ 04_data_analysis/994_scripts_from_emeka/TG_annotation.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#SBATCH -p c_vhighmem_dri1 ## dev, compute, htc, highmem
+#SBATCH --job-name=TG3_level1_ldsc_25k
+#SBATCH --ntasks=40
+#SBATCH --ntasks-per-node=40
+#SBATCH --array=1-22
+##### #SBATCH --mem-per-cpu=8000 # memory limit per core
+#SBATCH --mem=300G # memory limit per compute node for the job
+#SBATCH --time=4-00:00 # maximum job time in D-HH:MM
+#SBATCH --account=scw1329
+#SBATCH -o /scratch/.../%x_out_%A_%a_%J.txt
+#SBATCH -e /scratch/.../%x_err_%A_%a_%J.txt
+
+echo "*****************************************************************"
+echo "All jobs in this array have:"
+echo "- SLURM_ARRAY_JOB_ID: ${SLURM_ARRAY_JOB_ID}"
+echo "- SLURM_ARRAY_TASK_COUNT: ${SLURM_ARRAY_TASK_COUNT}"
+echo "- SLURM_ARRAY_TASK_MIN: ${SLURM_ARRAY_TASK_MIN}"
+echo "- SLURM_ARRAY_TASK_MAX: ${SLURM_ARRAY_TASK_MAX}"
+echo "This job in the array has:"
+echo "- SLURM_JOB_ID: ${SLURM_JOB_ID}"
+echo "- SLURM_ARRAY_TASK_ID: ${SLURM_ARRAY_TASK_ID}"
+echo "Run on host: "`hostname`
+echo "Number of threads (nproc): "`nproc`
+echo "Total memory in GB: "`free -g | grep -oP '\d+' | sed -n 1p`
+echo "Used memory in GB: "`free -g | grep -oP '\d+' | sed -n 2p`
+echo "Free memory in GB: "`free -g | grep -oP '\d+' | sed -n 3p`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo -e "*****************************************************************\n"
+
+module load ldsc/1.0.1
+module load bedtools/2.29.2
+
+
+N=${SLURM_ARRAY_TASK_ID}
+
+
+LDSC="/LDSC/ldsc"
+GENESET="/TG3_level1_GeneSet"
+PHASE3="/1000G_EUR_Phase3_plink"
+OUTPUT="path"
+ENSG ="/path"
+
+
+for file in Emeka_Fibroblast Emeka_Immune Emeka_Satglia Emeka_Schwann_M Emeka_Vascular Emeka_control_Fibroblast Emeka_control_Immune Emeka_control_Satglia Emeka_control_Schwann_M Emeka_control_Vascular Emeka_control_neuronal Emeka_neuronal
+do
+echo ${file}
+
+  python $LDSC/make_annot.py \
+  --gene-set-file $GENESET/$file.GeneSet \
+  --gene-coord-file $ENSG/ENSG_coord.txt \
+  --windowsize 25000 \
+  --bimfile $PHASE3/1000G.EUR.QC.$N.bim \
+  --annot-file $OUTPUT/$file.$N.annot.gz
+done
+
+
+echo -e "\n************************************************************"
+echo "Finished at: "`date`
+echo "*****************************************************************"
---
+++ 04_data_analysis/994_scripts_from_emeka/TG_calculate_LDSC_ldscore.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#SBATCH -p c_vhighmem_dri1 ## dev, compute, htc, highmem
+#SBATCH --job-name=level1_50k_ldscore
+#SBATCH --ntasks=40
+#SBATCH --ntasks-per-node=30
+##### #SBATCH --array=1-308%14
+##### #SBATCH --mem-per-cpu=8000 # memory limit per core
+#SBATCH --mem=360G # memory limit per compute node for the job
+#SBATCH --time=4-00:00 # maximum job time in D-HH:MM
+#SBATCH --account=scw1329
+#SBATCH -o /scratch/..../%x_out_%A_%a_%J.txt
+#SBATCH -e /scratch/..../%x_err_%A_%a_%J.txt
+
+echo "*****************************************************************"
+echo "All jobs in this array have:"
+echo "- SLURM_ARRAY_JOB_ID: ${SLURM_ARRAY_JOB_ID}"
+echo "- SLURM_ARRAY_TASK_COUNT: ${SLURM_ARRAY_TASK_COUNT}"
+echo "- SLURM_ARRAY_TASK_MIN: ${SLURM_ARRAY_TASK_MIN}"
+echo "- SLURM_ARRAY_TASK_MAX: ${SLURM_ARRAY_TASK_MAX}"
+echo "This job in the array has:"
+echo "- SLURM_JOB_ID: ${SLURM_JOB_ID}"
+echo "- SLURM_ARRAY_TASK_ID: ${SLURM_ARRAY_TASK_ID}"
+echo "Run on host: "`hostname`
+echo "Number of threads (nproc): "`nproc`
+echo "Total memory in GB: "`free -g | grep -oP '\d+' | sed -n 1p`
+echo "Used memory in GB: "`free -g | grep -oP '\d+' | sed -n 2p`
+echo "Free memory in GB: "`free -g | grep -oP '\d+' | sed -n 3p`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo -e "*****************************************************************\n"
+
+module load ldsc/1.0.1
+module load bedtools/2.29.2
+
+## input
+## gene sets
+
+N=${SLURM_ARRAY_TASK_ID}
+
+echo "Input dir: "$INPUT_DIR
+
+LDSC="/LDSC/ldsc"
+PHASE3="/1000G_EUR_Phase3_plink"
+ANNOT="/annotation_output_path"
+OUTPUT="path"
+HAPMAP3="/hapmap3_snps"
+
+
+for chrom in {1..22}
+do
+  echo ${chrom}
+for file in Emeka_Fibroblast Emeka_Immune Emeka_Satglia Emeka_Schwann_M Emeka_Vascular Emeka_control_Fibroblast Emeka_control_Immune Emeka_control_Satglia Emeka_control_Schwann_M Emeka_control_Vascular Emeka_control_neuronal Emeka_neuronal
+do
+echo ${file}
+  
+  python $LDSC/ldsc.py \
+  --l2 \
+  --bfile $PHASE3/1000G.EUR.QC.$chrom \
+  --ld-wind-cm 1 \
+  --annot $ANNOT/$file.$chrom.annot.gz \
+  --thin-annot \
+  --out $OUTPUT/$file.$chrom \
+  --print-snps $HAPMAP3/hm.$chrom.snp
+done
+done
+
+echo -e "\n************************************************************"
+echo "Finished at: "`date`
+echo "*****************************************************************"
