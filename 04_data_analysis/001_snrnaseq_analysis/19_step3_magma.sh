72bd3826a0692d9fb0fe3ab785cf6b4c986d6632
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2024-03-06 11:49:00 +0000

    Update magma input file extension
    I want to also run magma on gene lists from the PPI modules so this makes the code more generalisable

--- 04_data_analysis/001_snrnaseq_analysis/19_step3_magma.sh
+++ 04_data_analysis/001_snrnaseq_analysis/19_step3_magma.sh
@@ -40,8 +40,8 @@ WORK_DIR="/scratch/scw1329/gmbh/blood-brain-barrier-in-ad/03_data/994_magma_inpu
 cd $WORK_DIR
 export WORK_DIR
 
-# Populate Set_Annot_Files array with files matching 'magma_celltypes_*'
-Set_Annot_Files=(${WORK_DIR}/magma_celltypes_*)
+# Populate Set_Annot_Files array with files matching '*.magma.txt'
+Set_Annot_Files=(${WORK_DIR}/*.magma.txt)
 # Populate Gene_Results_Files array with files matching '*.genes.raw'
 Gene_Results_Files=(${WORK_DIR}/*.genes.raw)
 
@@ -49,9 +49,9 @@ Gene_Results_Files=(${WORK_DIR}/*.genes.raw)
 run_magma() {
     local Set_Annot_File=$1
     local Gene_Results_File=$2
-    local annot_name=$(basename "${Set_Annot_File}" .tsv)
+    local annot_name=$(basename "${Set_Annot_File}" .magma.txt)
     local gene_name=$(basename "${Gene_Results_File}" .genes.raw)
-    local Output_Prefix="./results/level2_celltypes_${annot_name}_${gene_name}"
+    local Output_Prefix="./results/${annot_name}_${gene_name}"
 
     # Run magma command
     magma \
