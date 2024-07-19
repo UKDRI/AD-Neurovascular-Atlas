d908a112e5469ce1cf400e0bc3f973b8ac856f8a
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2024-07-19 11:30:47 +0100

    readd HPC code

---
+++ 04_data_analysis/001_snrnaseq_analysis/00_hpc_variables.sh
@@ -0,0 +1,7 @@
+PROJECT_ROOT="/scratch/c.mpmgb/blood-brain-barrier-in-ad/"
+## This if for batch 1 set 1
+INPUT_DIR_b1s1="/gluster/dri02/rdscw/shared/webber/Endo_10X/FASTQ/Set_1/211008_A00748_0157_AHT7TJDSX2_fastq_L2_3_4/"
+## This is for batch 1 set 2
+INPUT_DIR_b1s2="/gluster/dri02/rdscw/shared/webber/Endo_10X/FASTQ/Set_2/"
+## This is the directory for batch 2 (set 3)
+INPUT_DIR="/gluster/dri02/rdsmbh/shared/rdsmbh/230327_A00748_0368_AH5CTMDSX5_fastq/"
