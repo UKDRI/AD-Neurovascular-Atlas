18aede8803f7d4749f766b1195724ff7a8a59e8e
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2024-10-27 19:14:49 +0000

    Add tradeseq code

---
+++ 04_data_analysis/990_code_libraries/run_tradeseq.R
@@ -0,0 +1,46 @@
+# load packages
+if (!require("Seurat", character.only = TRUE)) {
+  install.packages("Seurat")
+  library(Seurat)
+}
+if (!require("future", character.only = TRUE)) {
+  install.packages("future")
+  library(future)
+}
+if (!require("qs", character.only = TRUE)) {
+  install.packages("qs")
+  library(qs)
+}
+library(slingshot)
+library(tradeseq)
+
+# read data
+sce_slingshot <- qs::qread(sce_slingshot, here("03_data/990_processed_data/008_pseudotime",
+                              "slingshot_obj.qs"))
+
+sds <- SlingshotDataSet(sce_slingshot)
+# Set up parallel processing
+BPPARAM <- BiocParallel::bpparam()
+BPPARAM$workers <- parallel::detectCores()
+BPPARAM
+
+# Evaluate the optimal number of knots
+set.seed(123)  # For reproducibility
+k_values <- evaluateK(counts = assays(sce_slingshot)$counts, sds = sds, 
+                      nGenes = 500, k = 3:10, parallel = TRUE, BPPARAM = BPPARAM)
+k_values
+
+optimal_k <- which.min(k_values$ic$BIC)
+# Having a hard time running evaluateK, it's slow and seems to crash, I'll just try progressing with nknots of 6 (the default)
+# Fit GAMs for each gene along the pseudotime
+sce_slingshot <- fitGAM(counts = assays(sce_slingshot)$counts, pseudotime = slingPseudotime(sds, na = FALSE), cellWeights = slingCurveWeights(sds), nknots = optimal_k, parallel = TRUE, BPPARAM = BPPARAM)
+
+# Extract the model results
+gam_results <- rowData(sce_slingshot)$tradeSeq
+
+print("gam results head:")
+print(head(gam_results))
+
+# Save
+qs::qsave(sce_slingshot, here::here("03_data/990_processed_data/008_pseudotime/slingshot_tradeseq.qs"))
+qs::qsave(k_values, here::here("03_data/990_processed_data/008_pseudotime/slingshot_kvalues.qs"))
---
+++ 04_data_analysis/990_code_libraries/run_tradeseq.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH -p c_compute_dri1
+#SBATCH --job-name=run_tradeseq
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=120G # memory limit per compute node for the job
+#SBATCH --time=1-00:00 # maximum job time in D-HH:MM
+#SBATCH --account=scw1329
+#SBATCH -o /scratch/c.mpmgb/hawk_output/%x_out_%A_%a_%J.txt
+#SBATCH -e /scratch/c.mpmgb/hawk_output/%x_err_%A_%a_%J.txt
+#SBATCH --mail-user Bernardo-HarringtonG@cardiff.ac.uk # email on fail
+#SBATCH --mail-type END,FAIL
+
+# load environment
+# note mamba env created with the following:
+#mamba create --prefix r-env -c conda-forge -c bioconda -c R -c paul.martin-2 r-base r-seurat r-future r-readr r-remotes r-doubletfinder r-here r-tidyverse bioconductor-mast r-qs bioconductor-slingshot bioconductor-tradeseq
+
+# activate conda environment
+module load anaconda
+source activate
+
+# move to directory
+cd /scratch/scw1329/gmbh/blood-brain-barrier-in-ad
+
+conda activate /scratch/scw1329/gmbh/blood-brain-barrier-in-ad/r-env
+
+Rscript 04_data_analysis/990_code_libraries/run_tradeseq.R
