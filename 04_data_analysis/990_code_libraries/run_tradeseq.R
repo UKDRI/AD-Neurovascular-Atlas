1af762ed22167394ef2b78717dcb78201a282a98
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2024-11-01 15:34:26 +0000

    Add subsampling for tradeseq

--- 04_data_analysis/990_code_libraries/run_tradeseq.R
+++ 04_data_analysis/990_code_libraries/run_tradeseq.R
@@ -18,7 +18,27 @@ library(tradeSeq)
 sce_slingshot <- qs::qread(here::here("03_data/990_processed_data/008_pseudotime",
                               "slingshot_obj.qs"))
 
-sds <- SlingshotDataSet(sce_slingshot)
+# subsample to a very small number of cells to test code is working
+subsample_cells <- function(sce, celltype_col, n = 5000) {
+  cell_types <- unique(colData(sce)[[celltype_col]])
+  sampled_cells <- unlist(lapply(cell_types, function(ct) {
+    cells <- which(colData(sce)[[celltype_col]] == ct)
+    if (length(cells) > n) {
+      sample(cells, n)
+    } else {
+      cells
+    }
+  }))
+  return(sampled_cells)
+}
+
+celltype_col <- "level1_celltypes_with_endomt_subclusters"
+set.seed(1234)
+sampled_cells <- subsample_cells(sce_slingshot, celltype_col)
+sce_subsampled <- sce_slingshot[, sampled_cells]
+
+
+sds <- SlingshotDataSet(sce_subsampled)
 # Set up parallel processing
 BPPARAM <- BiocParallel::bpparam()
 BPPARAM$workers <- parallel::detectCores()
@@ -26,21 +46,34 @@ BPPARAM
 
 # Evaluate the optimal number of knots
 set.seed(123)  # For reproducibility
-k_values <- evaluateK(counts = assays(sce_slingshot)$counts, sds = sds, 
-                      nGenes = 500, k = 3:10, parallel = TRUE, BPPARAM = BPPARAM)
-k_values
+print("Running evaluateK...")
+k_values <- evaluateK(counts = assays(sce_subsampled)$counts, 
+                      sds = test$slingshot, nGenes = 500, k = 3:10, 
+                      parallel = TRUE, BPPARAM = BPPARAM)
+print("Finished evaluateK!")
 
+qs::qsave(k_values, here::here("03_data/990_processed_data/008_pseudotime/slingshot_kvalues.qs"))
+
+k_values
 optimal_k <- which.min(k_values$ic$BIC)
-# Having a hard time running evaluateK, it's slow and seems to crash, I'll just try progressing with nknots of 6 (the default)
 # Fit GAMs for each gene along the pseudotime
-sce_slingshot <- fitGAM(counts = assays(sce_slingshot)$counts, pseudotime = slingPseudotime(sds, na = FALSE), cellWeights = slingCurveWeights(sds), nknots = optimal_k, parallel = TRUE, BPPARAM = BPPARAM)
+print("Running fitGAM...")
+sce_subsampled <- fitGAM(
+  counts = assays(sce_subsample)$counts,
+  pseudotime = slingPseudotime(sds, na = FALSE),
+  cellWeights = slingCurveWeights(sds),
+  nknots = optimal_k,
+  parallel = TRUE,
+  BPPARAM = BPPARAM
+)
+print("Finished fitGAM!")
 
 # Extract the model results
 gam_results <- rowData(sce_slingshot)$tradeSeq
 
+# Save
+qs::qsave(sce_slingshot, here::here("03_data/990_processed_data/008_pseudotime/slingshot_tradeseq.qs"))
+
 print("gam results head:")
 print(head(gam_results))
 
-# Save
-qs::qsave(sce_slingshot, here::here("03_data/990_processed_data/008_pseudotime/slingshot_tradeseq.qs"))
-qs::qsave(k_values, here::here("03_data/990_processed_data/008_pseudotime/slingshot_kvalues.qs"))
